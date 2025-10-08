2259be04b17836c2b5ed24ce5fba4c8cf02c829f
okitavios1801@gmail.com <Otvio@DESKTOP-MTKKSDG>
2025-10-08 19:50:06 -0300

    Classe estudante e começo da classe ListaDeEstudantes

---
+++ GerenciadorAcademico/src/GerenciadorAcademico/Estudante.java
@@ -0,0 +1,12 @@
+package GerenciadorAcademico;
+
+public class Estudante {
+	private String nome;
+	private int id;
+	
+	private Estudante(String nome, int id) {
+		this.nome=nome;
+		this.id=id;
+	}
+
+}
--- GerenciadorAcademico/src/GerenciadorAcademico/ListaDeEstudantes.java
+++ GerenciadorAcademico/src/GerenciadorAcademico/ListaDeEstudantes.java
@@ -1,5 +1,19 @@
 package GerenciadorAcademico;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaDeEstudantes {
+	 public List<Estudante> lista;
+	 
+	 private ListaDeEstudantes() {
+		 this.lista=new ArrayList<>();
+	 }
+	 
+	 public void adicionarEstudante(Estudante e) {
+		 lista.add(e);
+	 }
+	 public void removerEstudantePorId(int id) {
+		 lista.remove(id);
+	 }
 
 }
