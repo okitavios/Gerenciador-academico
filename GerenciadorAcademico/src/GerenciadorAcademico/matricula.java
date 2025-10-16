fa695f0b61c4664da797c49eefce14d0c74bc9f4
lucas026-ligther <lucasesluz2@gmail.com>
2025-10-16 11:05:04 -0300

    Add files via upload
    Esta parte do codigo diz respeito ao programa de matriculas do sistema

---
+++ GerenciadorAcademico/src/GerenciadorAcademico/matricula.java
@@ -0,0 +1,37 @@
+public class matricula {
+    private String nome;
+    private int matricula;
+    private String turma;
+    private int anoLetivo;
+    private double notaMedia;
+    private String disciplina;
+
+    public matricula(String nome, int matricula, String turma, int anoLetivo, double notaMedia,  String disciplina) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.turma = turma;
+        this.anoLetivo = anoLetivo;
+        this.notaMedia = notaMedia;
+        this.disciplina = disciplina;
+    }
+        public String getnome(){
+           return nome;
+        }
+        public int getmatricula(){
+        return matricula;
+        }
+        public String getturma(){
+        return turma;
+        }
+        public int getanoLetivo(){
+        return anoLetivo;
+        }
+        public double getnotaMedia(){
+        return notaMedia;
+        }
+        public String getdisciplina(){
+        return disciplina;
+        }
+
+
+}
